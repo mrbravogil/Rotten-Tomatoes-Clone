6c7e21b53f6e255b21fe9c6c1c9cec8de6210b2c
mrbravogil <mrbravogil@gmail.com>
2025-07-01 11:56:51 +0200

    mini padding cambio

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -6,7 +6,7 @@ export default function MainLayout() {
   return (
     <div className="min-h-screen flex flex-col bg-gray-100 items-center justify-center">
       <Header className="shadow-lg" />
-      <main className="bg-white w-[80%] p-10 lg:p-20 shadow-lg">
+      <main className="bg-white w-[80%] p-5 lg:p-10 shadow-lg">
         <Outlet /> {/* Páginas */}
       </main>
       <Footer className="shadow-lg" />
